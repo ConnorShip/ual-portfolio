78207b51e8579ca337b0e54ecf26b940a5d8a673
Connor Shipley <connorshipley9@gmail.com>
2025-03-13 16:31:03 +0000

    add dynamic footer

---
+++ footer/footer.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const footer = document.createElement("footer");
+    footer.innerHTML = "<p>To Pursue Anything is to Risk Failure</p>";
+
+    document.getElementById("footer-container").appendChild(footer)
+});
