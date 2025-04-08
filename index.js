e9e884dbf55f9e3e21975ff88a0a39e8db1499b3
Connor Shipley <connorshipley9@gmail.com>
2025-04-08 12:35:19 +0100

    Fix back to relative

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("aboutBtn").addEventListener("click", () => {
-        window.location.href="/about/about.html";
+        window.location.href="./about/about.html";
     })
 });
